bdc55865dc0f921d0f4d2ad753c2c7ba8e1b912f
Siarhei Burakouski <serge.burakovsky@gmail.com>
2021-03-01 17:00:55 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length == 0) return [];
+  let flatten = matrix.reduce(function(merged, arr, index){ 
+    return merged.concat(arr.sort((a, b) => (index % 2 == 0)?(a - b):(b-a)))
+  });
+  return flatten;
 }
